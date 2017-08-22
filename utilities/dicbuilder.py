3ae684dcc4137efa946b90a080d494d9b162b2c6
omrilidor <omrilidor25@gmail.com>
2017-08-22 15:00:33 +0300

    creating a dictionary for all teams accourding to the names in seasons 2017 and combining all pairs of 2*** and 2***B tables to one table

--- utilities/dicbuilder.py
+++ utilities/dicbuilder.py
@@ -1,14 +1,24 @@
 import csv
 with open('2017.csv', newline='') as csvfile:
-    dataTable = csv.reader(csvfile, delimiter=',', quotechar='|')
+    dataTable1 = csv.reader(csvfile, delimiter=',', quotechar='|')
     teams =[]
-    for row in dataTable:
+    for row in dataTable1:
         if row[0] == 'Rk' or row[1] == 'League Average':
             continue
         teams.append(row[1])
-    sortedTeams = []
+    TeamID = []
     for i in range (1,31):
         team = (sorted(teams)).pop(0)
-        sortedTeams.append((i,team))
+        TeamID.append((i,team))
         teams.remove(team)
-    print(sortedTeams)
+    TeamID.append((31,'League Average'))
+    for i in range(0, 18):
+        fileName1 = str(2000 + i) + ".csv"
+        fileName2 = str(2000 + i)+ "B" + ".csv"
+        with open(fileName1, newline='') as csvfile:
+            dataTable1 = csv.reader(csvfile, delimiter=',', quotechar='|')
+        with open(fileName2, newline='') as csvfile:
+            dataTable2 = csv.reader(csvfile, delimiter=',', quotechar='|')
+        with open(fileName2, newline='') as csvfile:
+            dataWriter = csv.writer(csvfile, delimiter=',', quotechar='|')
+
