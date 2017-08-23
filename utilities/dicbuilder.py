cf559a6ec8fd587797712a378dc3b8c299eee302
omrilidor <omrilidor25@gmail.com>
2017-08-23 13:27:10 +0300

    just about to be done

--- utilities/dicbuilder.py
+++ utilities/dicbuilder.py
@@ -7,7 +7,7 @@ with open('2017.csv', newline='') as csvfile:
     dataTable = csv.reader(csvfile, delimiter=',', quotechar='|')
     dicTeams = []
     for row in dataTable:
-        if row[0] == 'Rk':
+        if row[0] == 'Rk' or row[1] == 'League Average':
             continue
         dicTeams.append(row[1])
     for i in range(1, 31):
@@ -24,13 +24,27 @@ for j in range(0, 18):
     ######################################################
     ### creating a key for each team per season ##########
     ### accourding to prime dictionary (2017) ############
+    teams = []
     with open(fileName1, newline='') as csvfile:
         dataTable1 = csv.reader(csvfile, delimiter=',', quotechar='|')
-        teams =[]
         for row in dataTable1:
             if row[0] == 'Rk':
                 continue
-            teams.append(row[1])
+            if row[1] in dicTeamID.keys():
+                teams.append((row[1], dicTeamID[row[1]]))
+                if 2000 + j < 2003:
+                    teams.append(('Charlotte Hornets', dicTeamID['New Orleans Pelicans']))
+            else:
+                if row[1] == 'Seattle SuperSonics':
+                    teams.append((row[1], dicTeamID['Oklahoma City Thunder']))
+                if row[1] == 'New Jersey Nets':
+                    teams.append((row[1], dicTeamID['Brooklyn Nets']))
+                if row[1] == 'Vancouver Grizzlies':
+                    teams.append((row[1], dicTeamID['Memphis Grizzlies']))
+                if row[1] == 'New Orleans Hornets':
+                    teams.append((row[1], dicTeamID['New Orleans Pelicans']))
+                if row[1] == 'New Orleans/Oklahoma City Hornets':
+                    teams.append((row[1], dicTeamID['New Orleans Pelicans']))
     ######################################################
     ######## merging tables a and b and adding key to ####
     ######## TeamID colomn ###############################
@@ -39,10 +53,9 @@ for j in range(0, 18):
     x = file1.merge(file2, on='Team')
     x['TeamID'] = 100
     for row1 in x:
-        if row1[0] == 'Rk':
-            continue
-        if row1[1] in dicTeamID.keys():
-            row1[53] = dicTeamID[row1[1]]
+        for franchise in teams:
+            if row1[1] == franchise[0]:
+                row1[53] = franchise[1]
     x.to_csv(fileName1, index = False)
 
 
