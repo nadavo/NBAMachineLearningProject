ead7be2918126044bb8d841321971992d250d7e3
omrilidor <omrilidor25@gmail.com>
2017-08-23 10:41:03 +0300

    creating a dictionary for all teams accourding to the names in seasons 2017 and combining all pairs of 2*** and 2***B tables to one table. adding a TeamID colomn and adding a team's unique key to each season acourding to a dictionary

--- utilities/dicbuilder.py
+++ utilities/dicbuilder.py
@@ -1,5 +1,7 @@
 import csv
 import pandas as pd
+#####################################################
+### creating a dictionary for each team #############
 with open('2017.csv', newline='') as csvfile:
     dataTable = csv.reader(csvfile, delimiter=',', quotechar='|')
     dicTeams = []
@@ -13,18 +15,15 @@ with open('2017.csv', newline='') as csvfile:
         dicTeamID.append((team, i))
         dicTeams.remove(team)
     dicTeamID.append(('League Average', 31))
-    print(dicTeamID)
+print(dicTeamID)
+#######################################################
+######## merging tables a and b and adding key colomn #
 for j in range(0, 18):
     fileName1 = str(2000 + j) + ".csv"
     fileName2 = str(2000 + j) + "B" + ".csv"
-    ##################################################
-    #### merging tables a and b ######################
-    '''file1 = pd.read_csv(fileName1)
-    file2 = pd.read_csv(fileName2)
-    x = file1.merge(file2, on='Team')
-    x.to_csv(fileName1, index = False)'''
-    ##################################################
-    ##################################################
+    ######################################################
+    ### creating a key for each team per season ##########
+    ### accourding to prime dictionary (2017) ############
     with open(fileName1, newline='') as csvfile:
         dataTable1 = csv.reader(csvfile, delimiter=',', quotechar='|')
         teams =[]
@@ -33,9 +32,15 @@ for j in range(0, 18):
                 continue
             teams.append(row[1])
         TeamID = []
-        for i in range (1,31):
-            team = (sorted(teams)).pop(0)
-            TeamID.append((team,i))
-            teams.remove(team)
-        TeamID.append(('League Average',31))
-        print(TeamID)
+    ######################################################
+    ###
+    ######################################################
+    ######## merging tables a and b and adding key to ####
+    ######## TeamID colomn ###############################
+    file1 = pd.read_csv(fileName1)
+    file2 = pd.read_csv(fileName2)
+    x = file1.merge(file2, on='Team')
+    x['TeamID']=5
+    x.to_csv(fileName1, index = False)
+
+
