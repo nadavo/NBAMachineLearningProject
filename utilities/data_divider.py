93902adc5f0effbec6b7e2a85740f57ec83941a2
omrilidor <omrilidor25@gmail.com>
2017-09-03 15:40:58 +0300

    fixing a mess i made

--- utilities/data_divider.py
+++ utilities/data_divider.py
@@ -6,6 +6,8 @@ times = [0,1,2]
 
 file = pd.read_csv('31structured_next.csv')
 for time in times:
+    #########################################
+    ### divide by seasons and conference ####
     x = file
     y = file
     for j, row in file.iterrows():
@@ -22,4 +24,27 @@ for time in times:
                 continue
     x.to_csv(str(years[time])+'East.csv', index=False)
     y.to_csv(str(years[time])+'west.csv', index=False)
+
+    #############################################
+    ### divide by conference ####################
+    east = file
+    west = file
+    if time == 0:
+        for j, row in file.iterrows():
+            if row['Season']==0:
+                west = west.drop(file.index[j])
+            else:
+                east = east.drop(file.index[j])
+        east.to_csv('East.csv', index=False)
+        west.to_csv('West.csv', index=False)
+
+    ##########################################
+    ### divide by seasons ####################
+
+    season = file
+    for j, row in file.iterrows():
+        if row['Season'] > years[time]+16 or row['Season'] < years[time]:
+            season = season.drop(file.index[j])
+
+    season.to_csv(str(years[time])+".csv", index=False)
 
