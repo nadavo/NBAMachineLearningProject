08895cbbe8298faa79d729ca8f04be85b4c3a709
omrilidor <omrilidor25@gmail.com>
2017-09-03 19:32:40 +0300

    added binary dataset creator (work in progress)

--- utilities/data_divider.py
+++ utilities/data_divider.py
@@ -31,7 +31,7 @@ for time in times:
     west = file
     if time == 0:
         for j, row in file.iterrows():
-            if row['Season']==0:
+            if row['E/W']==0:
                 west = west.drop(file.index[j])
             else:
                 east = east.drop(file.index[j])
@@ -48,3 +48,23 @@ for time in times:
 
     season.to_csv(str(years[time])+".csv", index=False)
 
+    ########################################################
+    ### create binary data files for 18, 24, 31 seasons ####
+
+    binary = file
+    for j, row in file.iterrows():
+        if row['Season'] < years[time]:
+            binary = binary.drop(file.index[j])
+        if row['Standings_Bucket']==1:
+            binary = binary.set_value(index=j, col='Standings_Bucket', value=0)
+        if row['Standings_Bucket']==2:
+            print(time)
+            binary = binary.set_value(index=j, col='Standings_Bucket', value=1)
+        if row['Standings_Bucket_Next']==1:
+            binary = binary.set_value(index=j, col='Standings_Bucket_Next', value=0)
+        if row['Standings_Bucket_Next']==2:
+            binary = binary.set_value(index=j, col='Standings_Bucket_Next', value=1)
+            print(time)
+
+    name = int(2017-years[time])
+    binary.to_csv(str(name)+'Binary.csv')
